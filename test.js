14720d6daf06df246a39dccf316c06d29d3f0538
glinskyoff <omsk.den@bk.ru>
2024-06-14 18:23:11 +0600

    fix

---
+++ test.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.use("/", (req, res) => {
+  res.send("server is running.");
+});
+
+app.listen(5000, console.log("server started on PORT 5000"));
